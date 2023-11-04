4121f685f8cb805f3e1892f4dc68b4bd8d0d0a33
kazyhanov_ri <kazyhanov_ri@nlmk.com>
2023-11-04 16:28:03 +0300

    refactor: folders names cases fixed

---
+++ src/app/components/paginator/Paginator.container.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import PaginatorView from "./Paginator.view";
+
+
+type Props = {
+  page: number;
+  route: string;
+  pagesCount: number;
+};
+
+export default function PaginatorContainer({ page, route, pagesCount }: Props) {
+  const isFirstPage = page == 1;
+  const isLastPage = page == pagesCount;
+  return (
+    <PaginatorView
+      isFirstPage={isFirstPage}
+      isLastPage={isLastPage}
+      route={route}
+      page={page}
+      pagesCount={pagesCount}
+    />
+  );
+}
---
+++ src/app/components/paginator/Paginator.view.tsx
@@ -0,0 +1,45 @@
+import { useRouter } from 'next/navigation'
+
+type Props = {
+  isFirstPage: boolean;
+  isLastPage: boolean;
+  route: string,
+  page: number;
+  pagesCount: number;
+};
+
+export default function PaginatorView({
+  isFirstPage,
+  isLastPage,
+  route,
+  page,
+  pagesCount,
+}: Props) {
+  const router = useRouter()
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex gap-2">
+        <button
+          onClick={() => router.push(`${route}?page=${Math.max(1, page - 1)}`)}
+          disabled={isFirstPage}
+          className="rounded-lg p-4 bg-gray-900 max-w-screen-xl mx-auto"
+        >
+          Back
+        </button>
+
+        <div>
+          {page} / {pagesCount}
+        </div>
+
+        <button
+          onClick={() => router.push(`${route}?page=${Math.min(page + 1, pagesCount)}`)}
+          disabled={isLastPage}
+          className="rounded-lg p-4 bg-gray-900 max-w-screen-xl mx-auto"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
