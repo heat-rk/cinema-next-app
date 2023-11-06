bb8ba7fedf8c6c9019ed76f1dd6ffb8f046c3d22
kazyhanov_ri <kazyhanov_ri@nlmk.com>
2023-11-06 19:05:23 +0300

    feature: movie list item redesign

--- src/app/modules/movies/ui/movies_list/MovieListItem.view.tsx
+++ src/app/modules/movies/ui/movies_list/MovieListItem.view.tsx
@@ -13,19 +13,19 @@ export default function MovieListItemView({ movie }: Props) {
           <img
             src={movie.medium_cover_image}
             alt={movie.title}
-            className="w-16 h-auto"
+            className="w-24 h-auto rounded-lg"
           />
         </div>
       </Link>
       <div className="flex-1">
         <Link href={`/movies/${movie.id}`}>
-          <div className="text-lg font-semibold mb-1">{movie.title}</div>
+          <div className="text-xl font-semibold mb-1">{movie.title}</div>
         </Link>
-        <div className="text-sm text-gray-500 mb-1">{movie.year}</div>
-        <div className="text-sm text-gray-500">
+        <div className="text-lg text-gray-500">{movie.year}</div>
+        <div className="text-lg text-gray-500">
           Genres: {movie.genres.join(", ")}
         </div>
-        <div className="text-sm text-gray-500">
+        <div className="text-lg text-gray-500">
           Rating: {movie.rating === 0 ? "Brand New" : movie.rating}
         </div>
       </div>
